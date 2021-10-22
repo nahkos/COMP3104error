8510db21e7b3e624365727c966d5269dd70b954b
nahkos <sokhan.prum78@gmail.com>
2021-10-21 23:57:47 -0400

    added missing folders to build

---
+++ build/precache-manifest.bcac437fae682e766f9454632333121e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f62a63fd49a8de8c1541",
+    "url": "/nahkos/COMP3104/static/css/main.7552f8ba.chunk.css"
+  },
+  {
+    "revision": "f62a63fd49a8de8c1541",
+    "url": "/nahkos/COMP3104/static/js/main.f62a63fd.chunk.js"
+  },
+  {
+    "revision": "c29b3d78654a36f56d7e",
+    "url": "/nahkos/COMP3104/static/js/runtime~main.c29b3d78.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/nahkos/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "3e7559ce98285e1ae4b683fc0693af3f",
+    "url": "/nahkos/COMP3104/index.html"
+  }
+];
